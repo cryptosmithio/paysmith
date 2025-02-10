6fcc1ca130c5751d3ff5f047532bbab98e4b91d7
Crypto Joe <1575077+cryptosmithio@users.noreply.github.com>
2025-02-10 11:51:46 +1000

    Bitcart integration working

--- lib/server/bitcart/config.sh
+++ lib/server/bitcart/config.sh
@@ -3,8 +3,7 @@
 #export BITCART_HOST=bitcart1.champagneroom.app
 export BITCART_CRYPTOS=eth
 export ETH_NETWORK=sepolia
-export ETH_SERVER=https://sepolia.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161
+export ETH_SERVER=https://mainnet.infura.io/v3/a2ee3a2384ad4a6f98bb3ff8dc7063a4
 export BITCART_EXCLUDE_COMPONENTS=store
-
-export BITCART_HOST=compose.orb.local
+export BITCART_HOST=host.docker.internal
 export BITCART_REVERSEPROXY="nginx"
