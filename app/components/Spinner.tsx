c46490d2f024c3f56d41a8dc8131b00a2c7a1dc9
Crypto Joe <1575077+cryptosmithio@users.noreply.github.com>
2025-02-18 07:00:37 +1000

    Enhanced Spinner component with props
    The Spinner component has been updated to accept additional properties. This allows for more flexibility and customization when using the component. The changes include importing necessary types from 'react' and '@chakra-ui/react', and spreading these props in the VStack element within the Spinner component.

--- app/components/Spinner.tsx
+++ app/components/Spinner.tsx
@@ -1,8 +1,22 @@
-import { Spinner as ChakraSpinner, Text, VStack } from '@chakra-ui/react';
+import {
+  Spinner as ChakraSpinner,
+  Text,
+  VStack,
+  type StackProps,
+} from '@chakra-ui/react';
+import type { JSX, RefAttributes } from 'react';
 
-export const Spinner = () => {
+export const Spinner = (
+  props: JSX.IntrinsicAttributes & StackProps & RefAttributes<HTMLDivElement>
+) => {
   return (
-    <VStack colorPalette="teal" w={'vw'} h={'vh'} justifyContent={'center'}>
+    <VStack
+      colorPalette="teal"
+      w={'vw'}
+      h={'vh'}
+      justifyContent={'center'}
+      {...props}
+    >
       <ChakraSpinner color="colorPalette.600" />
       <Text color="colorPalette.600">Loading...</Text>
     </VStack>
